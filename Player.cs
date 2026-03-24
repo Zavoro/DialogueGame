08aaf113136e32b16d16b68ca28b2f52400735c4
Zavier Woronowicz <zavierworo@gmail.com>
2026-03-24 14:06:33 +0000

    feat: add Player class with name and health fields
    closes #4

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DialogueGame
+{
+    public class Player
+    {
+        string name = "Unknown";
+        int health = 100;
+        //inventory - to be implemented in Issue #6  
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.health = 100;
+        }
+    }
+
+    
+}
